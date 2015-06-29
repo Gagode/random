39a8cdfcaafdafd3a92806a9ea9930e15239c2a8
Gabriel González Duarte <gagode15@gmail.com>
2015-06-28 21:50:57 -0400

    random versión 2

--- random.js
+++ random.js
@@ -1,11 +1,8 @@
 //Math.rand() devuelve un numero aleatorio entre 0 y 1
 var num = Math.random();
-
+var str = ' Mayor que 0,5';
 if(num <= 0.5)
 {
-  console.log('/n'+num+' Menor a 0.5 \n');
+    str = 'Menor que 0,5';
 }
-else
-{
-  console.log('/n'+num+' Menor a 0.5 \n');
-}
+console.log('/n'+ num+ str +'\n');
